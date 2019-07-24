016de0a0700a36b341c16cbc5745d90383218174
brookmezgu <brookmezgebu64@gmail.com>
2019-07-24 16:26:20 +0300

    feature: add activity for showing team information

---
+++ app/src/main/java/app/kuwas/android/ui/activities/TeamInformation.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2019 Brook Mezgebu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package app.kuwas.android.ui.activities;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.appbar.AppBarLayout;
+
+import app.kuwas.android.App;
+import app.kuwas.android.R;
+import app.kuwas.android.ui.adapters.TeamInformationAdapter;
+import io.brookmg.soccerethiopiaapi.data.Team;
+
+public class TeamInformation extends AppCompatActivity {
+
+    RecyclerView mainContentRecyclerView;
+    AppBarLayout mainAppBar;
+    AppCompatImageView backButton, teamIcon;
+    AppCompatTextView teamFullName;
+    Team currentTeamShowing;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_information);
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+//            getWindow().getDecorView().setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+//                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+//            );
+
+        teamFullName = findViewById(R.id.team_name);
+        backButton = findViewById(R.id.back_button);
+        teamIcon = findViewById(R.id.team_icon);
+        mainAppBar = findViewById(R.id.top_bar_container);
+        mainContentRecyclerView = findViewById(R.id.main_content_recycler_view);
+
+        try {
+            if (getIntent().getSerializableExtra("team") != null) {
+                currentTeamShowing = ((Team) getIntent().getSerializableExtra("team"));
+            }
+        } catch (NullPointerException exception) {
+            exception.printStackTrace();
+        }
+
+        teamFullName.setText(currentTeamShowing.getTeamFullName());
+        Glide.with(teamIcon).load(currentTeamShowing.getTeamLogo()).into(teamIcon);
+        backButton.setOnClickListener( v -> onBackPressed());
+        ViewCompat.setElevation(mainAppBar, 8);
+        fetchCompleteDetailOfTeam(currentTeamShowing);
+
+        mainContentRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private void fetchCompleteDetailOfTeam(Team team) {
+        if (team == null) return;
+
+        App.getInstance().getApi().getTeamDetail(team, completeTeam -> {
+            currentTeamShowing = completeTeam;
+            setupViews();
+        } , error -> Log.e("Team_Info" , error));
+    }
+
+    private void setupViews() {
+        mainContentRecyclerView.setAdapter(new TeamInformationAdapter(currentTeamShowing));
+    }
+
+}
---
+++ app/src/main/java/app/kuwas/android/ui/adapters/TeamInformationAdapter.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (C) 2019 Brook Mezgebu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package app.kuwas.android.ui.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import app.kuwas.android.R;
+import io.brookmg.soccerethiopiaapi.data.Team;
+
+import static app.kuwas.android.utils.Utils.dpToPx;
+
+/**
+ * Created by BrookMG on 6/28/2019 in app.kuwas.android.ui.adapters
+ * inside the project Kuwas .
+ */
+public class TeamInformationAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final int HEADER = 0;
+    private final int BODY = 1;
+
+    private final List<String> detailTitleStrings = Arrays.asList(
+            "Since",
+            "From City",
+            "Previous Names",
+            "Stadium",
+            "President",
+            "V.President",
+            "Manager",
+            "Coach",
+            "V.Coach",
+            "Technique Director",
+            "Goal Keeper",
+            "Team Alpha",
+            "Nurse",
+            "Keywords"
+    );
+
+    private List<List<String>> detailContentStrings;
+
+    public TeamInformationAdapter(Team team) {
+        detailContentStrings = feedTeamDetailsIntoArray(team);
+    }
+
+    private List<List<String>> feedTeamDetailsIntoArray(Team team) {
+        List<List<String>> returnable = new ArrayList<>();
+
+        returnable.add(Collections.singletonList(Integer.toString(team.getInitYear())));
+        returnable.add(Collections.singletonList(team.getFromCity()));
+        returnable.add(team.getPreviousNames());
+        returnable.add(Collections.singletonList(team.getStadium()));
+        returnable.add(Collections.singletonList(team.getPresident()));
+        returnable.add(Collections.singletonList(team.getVicePresident()));
+        returnable.add(Collections.singletonList(team.getManager()));
+        returnable.add(Collections.singletonList(team.getMainCoach()));
+        returnable.add(Collections.singletonList(team.getViceCoach()));
+        returnable.add(Collections.singletonList(team.getTechniqueDirector()));
+        returnable.add(Collections.singletonList(team.getGoalKeeper()));
+        returnable.add(Collections.singletonList(team.getTeamAlpha()));
+        returnable.add(Collections.singletonList(team.getTeamNurse()));
+        returnable.add(team.getKeywords());
+
+        return returnable;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case HEADER:
+                return new HeaderViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.blank_header, parent, false));
+            case BODY:
+            default:
+                return new ViewHolder(
+                        LayoutInflater.from(parent.getContext()).inflate(R.layout.team_information_element, parent, false)
+                );
+        }
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? HEADER : BODY;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == BODY) {
+            position = position -1;
+            ((ViewHolder) holder).detailTitle.setText(detailTitleStrings.get(position));
+            if (detailContentStrings.get(position).size() == 1) {
+                ((ViewHolder) holder).detailContent.setText(detailContentStrings.get(position).get(0));
+            } else {
+                ((ViewHolder) holder).detailRecyclerContent.setLayoutManager(
+                        new LinearLayoutManager(
+                                ((ViewHolder) holder).detailRecyclerContent.getContext(),
+                                RecyclerView.HORIZONTAL,
+                                false
+                        )
+                );
+
+                ((ViewHolder) holder).detailRecyclerContent.setAdapter(
+                        new TagsChipRecyclerAdapter(
+                                detailContentStrings.get(position)
+                        )
+                );
+
+                ((ViewHolder) holder).detailRecyclerContent.setVisibility(View.VISIBLE);
+                ((ViewHolder) holder).detailContent.setVisibility(View.GONE);
+            }
+        } else {
+            ViewGroup.LayoutParams params = ((HeaderViewHolder) holder).blankView.getLayoutParams();
+            params.height = dpToPx(holder.itemView.getContext(), 50);
+            ((HeaderViewHolder) holder).blankView.setLayoutParams(params);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return detailContentStrings.size() +1;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        AppCompatTextView detailTitle , detailContent;
+        RecyclerView detailRecyclerContent;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            detailContent = itemView.findViewById(R.id.detail_content);
+            detailRecyclerContent = itemView.findViewById(R.id.detail_recycler);
+            detailTitle = itemView.findViewById(R.id.detail_title);
+        }
+    }
+
+    class HeaderViewHolder extends RecyclerView.ViewHolder {
+        View blankView;
+
+        HeaderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            blankView = itemView.findViewById(R.id.blank_view);
+        }
+    }
+
+
+}
